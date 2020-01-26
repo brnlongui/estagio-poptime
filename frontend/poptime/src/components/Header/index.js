e4fd4256e723f11d4ddcec8711547a99fd5fb49e
brnlongui1 <bruninho_longui@hotmail.com>
2020-01-26 15:54:07 -0400

    criando o front

---
+++ frontend/poptime/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../Header/styles.css';
+const Header = () => (
+    <header id="main-header">PopTime
+    <button id="btn-perfil">Perfil</button>
+    </header>
+);
+
+export default Header;
