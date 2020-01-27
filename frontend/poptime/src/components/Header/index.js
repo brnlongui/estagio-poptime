008177565a7f0ac28c31356a7bef280354154ea3
brnlongui1 <bruninho_longui@hotmail.com>
2020-01-26 21:52:30 -0400

    teste

--- frontend/poptime/src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../Header/styles.css';
-const Header = () => (
-    <header id="main-header">PopTime
-    <button id="btn-perfil">Perfil</button>
-    </header>
-);
-
-export default Header;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const  cors = require ('cors');
 const mongoose = require('mongoose');
@@ -6,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@estagio-jera-sbvla.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useFindAndModify : false,
     useCreateIndex: true,
@@ -17,4 +18,4 @@ requireDir('./models');
 
 app.use('/api', require('./routes'));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
