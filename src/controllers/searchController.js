48b257195486f0f12226239bccb768b938b4a6e3
brnlongui1 <brunorlongui@gmail.com>
2020-08-02 15:44:16 -0400

    add gitignore

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -7,20 +7,13 @@ const Film = mongoose.model('Film');
 module.exports = {
     async search(req, res){
         const {term} = req.body;
-        let value = 2;//await req.body;
-        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1bcc763d1279f4d2d3cb5a06f1093374&language=pt-BR&query=${term}&page=${value}&include_adult=false`)
+        let value = 1;//await req.body;
+        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1bcc763d1279f4d2d3cb5a06f1093374&language=pt-BR&query=${term}&page=${value}&include_adult=false`);
 
-        return res.json(response.data)
-    }
-    // async show(req,res){
-    //     const {id, title, overview, year, poster_path} = response.data;
-    //     const film = await Film.show({
-    //         id,
-    //         title,
-    //         overview,
-    //         year,
-    //         poster_path
-    //     })
-    //     return res.json(film.id);
+        return res.json(response.data);
+    },
+    // async selected(req,res){
+    //     const obj = req.body;
+    //     const {id} =
     // }
 }
---
+++ src/controllers/watchController.js
@@ -0,0 +1,8 @@
+const Film = require('../models/film');
+const User = require('../models/profile');
+
+module.exports = {
+    store(){
+        const 
+    }
+}
