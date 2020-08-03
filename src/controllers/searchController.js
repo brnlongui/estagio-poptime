96e2e38dc2ca9e65fd86fb75262a75849fcb0139
brnlongui1 <brunorlongui@gmail.com>
2020-08-02 23:14:15 -0400

    controller de busca retornando a prop correta

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -7,13 +7,9 @@ const Film = mongoose.model('Film');
 module.exports = {
     async search(req, res){
         const {term} = req.body;
-        let value = 1;//await req.body;
-        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1bcc763d1279f4d2d3cb5a06f1093374&language=pt-BR&query=${term}&page=${value}&include_adult=false`);
-
-        return res.json(response.data);
-    },
-    // async selected(req,res){
-    //     const obj = req.body;
-    //     const {id} =
-    // }
+        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1bcc763d1279f4d2d3cb5a06f1093374&language=pt-BR&query=${term}&include_adult=false`);
+        const id = response.data.results[0].id;
+        
+        return res.json(id);
+    }
 }
